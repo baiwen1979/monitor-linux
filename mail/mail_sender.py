2de892e8299270095b2391625b8d676623897283
白文江 <baiwen1979@qq.com>
2020-07-31 13:41:18 +0800

    Update and rename send_mails.py to mail_sender.py

--- mail/mail_sender.py
+++ mail/mail_sender.py
@@ -1,11 +1,11 @@
 # coding = utf-8
 
 """
-@author: sy
+@author: baiwen1979
 
-@file: send_mails.py
+@file: mail_sender.py
 
-@time: 2018/9/22 16:55
+@time: 2020/07/30 16:55
 
 @desc: 发送邮件模块
 
@@ -15,18 +15,18 @@ import smtplib
 from email.header import Header
 from email.mime.text import MIMEText
 
-from config import gol
+from config import gconfig
 
 
-class SendMail(object):
+class MailSender(object):
     def __init__(self):
         """ 初始化邮箱模块 """
         try:
-            self.mail_host = gol.get_value("mail_host")  # 邮箱服务器
-            self.mail_port = gol.get_value("mail_port")  # 邮箱服务端端口
-            self.mail_user = gol.get_value("mail_user")  # 邮箱用户名
-            self.mail_pwd = gol.get_value("mail_pwd")  # 邮箱密码
-            self.mail_receivers = gol.get_value("mail_receivers").split(',')  # 收件人,以逗号分隔成列表
+            self.mail_host = gconfig.get_value("mail_host")  # 邮箱服务器
+            self.mail_port = gconfig.get_value("mail_port")  # 邮箱服务端端口
+            self.mail_user = gconfig.get_value("mail_user")  # 邮箱用户名
+            self.mail_pwd = gconfig.get_value("mail_pwd")  # 邮箱密码
+            self.mail_receivers = gconfig.get_value("mail_receivers").split(',')  # 收件人,以逗号分隔成列表
             smtp = smtplib.SMTP()
             smtp.connect(self.mail_host, self.mail_port)
             smtp.login(self.mail_user, self.mail_pwd)
@@ -34,11 +34,11 @@ class SendMail(object):
         except:
             print('发邮件---->初始化失败!请检查用户名和密码是否正确!')
 
-    def send_mails(self, content):
+    def send(self, content):
         """ 发送邮件 """
         try:
             message = MIMEText(content, 'plain', 'utf-8')
-            message['From'] = Header("巡检机器人小咪", 'utf-8')
+            message['From'] = Header("巡检机器人小白", 'utf-8')
             message['To'] = Header("系统警告", 'utf-8')
             subject = '各系统巡检信息'
             message['Subject'] = Header(subject, 'utf-8')
@@ -47,6 +47,6 @@ class SendMail(object):
         except Exception as e:
             print('发邮件---->失败!原因:', e)
 
-    def mail_close(self):
+    def close(self):
         """ 关闭邮箱资源 """
         self.smtp.close()
